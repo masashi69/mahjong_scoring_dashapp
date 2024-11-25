b1b0e731de2b00999bbda2e14fdbe36799bf2b1e
masashi suzuki <smasa619@gmail.com>
2024-11-25 12:45:35 +0900

    Added Valiables

--- syukei.py
+++ syukei.py
@@ -5,10 +5,17 @@ mj_df = pd.read_csv('2024_majong_score.csv')
 
 def main():
     for x in mj_df.groupby('player'):
+        player_score = x[1]['score']
+        player_rank = x[1]['rank']
+        p_average = player_score.mean()
+        p_max = player_score.max()
+        p_min = player_score.min()
+        p_rank = player_rank.mean()
         print(f'{x[0]}')
         print(f'    対局数: {len(x[1])}')
-        print(f"    スコア: {x[1]['score'].agg(['mean', 'max', 'min'])}")
-        print(f"    平均順位: {x[1]['rank'].agg(['mean'])}")
+        print(f"    平均スコア: {p_average}")
+        print(f"    最高スコア: {p_max}")
+        print(f"    平均順位: {p_rank}")
 
 if __name__ == '__main__':
     main()
