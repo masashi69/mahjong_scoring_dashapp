93acde1681397526cb4e96ed4aafde03584a3fd7
masashi suzuki <smasa619@gmail.com>
2024-11-25 12:21:24 +0900

    First commit

---
+++ syukei.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+mj_df = pd.read_csv('2024_majong_score.csv')
+
+for x in mj_df.groupby('player'):
+    print(f'{x[0]}')
+    print(f'    対局数: {len(x[1])}')
+    print(f"    スコア: {x[1]['score'].agg(['mean', 'max', 'min'])}")
+    print(f"    平均順位: {x[1]['rank'].agg(['mean'])}")
