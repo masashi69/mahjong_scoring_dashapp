6522999dfd1579d299f511771e6d777a7a5a8667
masashi suzuki <smasa619@gmail.com>
2024-12-05 15:39:48 +0900

    Fine tuned valiables

--- syukei.py
+++ syukei.py
@@ -4,22 +4,20 @@ mj_df = pd.read_csv('2024_majong_score.csv')
 
 
 def main():
-    for x in mj_df.groupby('player'):
-        player_score = x[1]['score']
-        player_rank = x[1]['rank']
-        p_average = player_score.mean()
-        p_max = player_score.max()
-        p_rank = player_rank.mean()
+    for ply in mj_df.groupby('player'):
+        p_avg_score, p_avg_rank = ply[1][['score', 'rank']].mean()
+        p_max = ply[1]['score'].max()
 
         # Round
-        p_average = round(p_average, 2)
+        p_average = round(p_avg_score, 2)
+        p_rank = round(p_avg_rank, 2)
         p_max = round(p_max, 2)
-        p_rank = round(p_rank, 2)
-        print(f'{x[0]}')
-        print(f'    対局数: {len(x[1])}')
+
+        print(f'{ply[0]}')
+        print(f'    対局数: {len(ply[1])}')
         print(f"    平均スコア: {p_average}")
         print(f"    最高スコア: {p_max}")
-        print(f"    平均順位: {p_rank}")
+        print(f"    平均順位  : {p_rank}")
 
 if __name__ == '__main__':
     main()
