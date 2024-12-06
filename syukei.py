ef55e064287fa985cd237dff278ef993f06c6731
masashi suzuki <smasa619@gmail.com>
2024-12-06 14:40:44 +0900

    Added rank to calculation of T-score

--- syukei.py
+++ syukei.py
@@ -2,14 +2,23 @@ import pandas as pd
 
 mj_df = pd.read_csv('2024_majong_score.csv')
 average_score = mj_df['score'].mean()
+average_rank  = mj_df['rank'].mean()
+
+# To calcurate deviation of rank
+average_rank  = abs(4 - average_rank)
 
 def main():
     for ply in mj_df.groupby('player'):
         p_avg_score, p_avg_rank = ply[1][['score', 'rank']].mean()
         p_max = ply[1]['score'].max()
 
+        # To calcurate deviation of rank
+        p_avg_rank_abs = abs(4 - p_avg_rank)
+
         # T-score
-        TScore = (p_avg_score - average_score) / mj_df['score'].std() * 10 + 50
+        TScore_s = (p_avg_score - average_score ) / mj_df['score'].std() * 10 + 50
+        TScore_r = (p_avg_rank_abs  - average_rank ) / mj_df['rank'].std() * 10 + 50
+        TScore = (TScore_s + TScore_r) / 2
 
         # Round
         p_average = round(p_avg_score, 2)
@@ -22,7 +31,7 @@ def main():
         print(f"    平均スコア: {p_average}")
         print(f"    最高スコア: {p_max}")
         print(f"    平均順位  : {p_rank}")
-        print(f"    偏差値    : {TScore}")
+        print(f"    スコア&ランク偏差値: {TScore}")
 
 if __name__ == '__main__':
     main()
